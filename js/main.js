a110b2d4922ef400cb26631dfa85e6507ad91c1e
Karina Lima <karinacomk.lima@gmail.com>
2020-07-09 09:56:13 -0300

    Concluindo

--- js/main.js
+++ js/main.js
@@ -23,3 +23,26 @@ function linkAction() {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=== scroll reveal animation ===*/
+
+const sr = ScrollReveal ({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.home__title', {})
+sr.reveal('.button', {delay: 200})
+sr.reveal('.home__img', {delay: 400})
+sr.reveal('.home__social-icon', {interval: 200})
+
+sr.reveal('.about__img', {})
+sr.reveal('.about__subtitle', {delay: 200})
+sr.reveal('.about__text', {delay: 400})
+
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__text', {delay: 200})
+sr.reveal('.skills__data', {interval: 200})
+sr.reveal('.skills__img', {delay: 400})
