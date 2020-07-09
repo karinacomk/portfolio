8532a326946956a4741294ef7236cd16fecafbd9
Karina Lima <karinacomk.lima@gmail.com>
2020-07-09 02:23:30 -0300

    home e about

--- js/main.js
+++ js/main.js
@@ -10,3 +10,16 @@ const showMenu = (toggleId, navId) => {
 }
 
 showMenu('nav-toggle','nav-menu')
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
